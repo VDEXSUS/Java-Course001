160737c624a0036ddb16defe28d6e0b489d5fed8
Vinc Dr <92882325+VDEXSUS@users.noreply.github.com>
2022-04-22 10:53:22 +0300

    Add files via upload

---
+++ Level003/Lesson3Example.java
@@ -0,0 +1,20 @@
+public class Lesson3Example {
+    public static void main(String[] args) {
+        // int value = 5;
+        // double value2 = 3;
+        // double sum = value % value2;
+        // System.out.println(sum);
+
+        //int value = 5;
+        //value = value + 25;
+        //System.out.println(value);
+
+        int value = 10;
+        value = value + 1;
+        value += 1;
+        value++;
+        System.out.println(value);
+        value--;
+        System.out.println(value);
+    }
+}
