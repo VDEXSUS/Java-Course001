8bb7edc93aeabbc87999be6e79a8f809acdbcfbf
Vinc Dr <92882325+VDEXSUS@users.noreply.github.com>
2022-04-24 09:32:25 +0300

    level005
    main

---
+++ level005/Level5Example.java
@@ -0,0 +1,21 @@
+package level005;
+
+public class Level5Example {
+
+    public static void main(String[] args) {
+        int value = 2000;
+        isLeapYear(value);
+    }
+
+    public static boolean isLeapYear(int year) {
+         return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+         //    return true;
+         // } else {
+         //    return false;
+         // }
+    }
+
+
+
+
+}
