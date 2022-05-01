79fbf0b263ce9516ebb8dd94bac856cc261dfbe4
Vinc Dr <92882325+VDEXSUS@users.noreply.github.com>
2022-05-01 09:28:06 +0300

    Add files via upload

---
+++ level006/RexursionExample.java
@@ -0,0 +1,18 @@
+package level006;
+
+public class RexursionExample {
+
+    public static void main(String[] args) {
+        // 1 * 2 * 3 * 4 * 5 * 6 = 6!
+        int factorial = factorial(5);
+        System.out.println(factorial);
+    }
+
+    public static int factorial(int value) {
+        if (value == 1) {
+            return 1;
+        }
+//        6 * (5 *(4 *(3 *(2 *(1))))
+        return value * factorial(value - 1);
+    }
+}
