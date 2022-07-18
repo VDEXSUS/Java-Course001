5eaff323909123cae0167e51d99969e3579d36d6
Vinc Dr <92882325+VDEXSUS@users.noreply.github.com>
2022-07-18 22:34:09 +0300

    Add files via upload

---
+++ level009/Ram.java
@@ -0,0 +1,20 @@
+package com.vincas.oop.level009;
+
+public class Ram {
+
+    private int value;
+
+    public Ram(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+}
---
+++ level009/Ssd.java
@@ -0,0 +1,18 @@
+package com.vincas.oop.level009;
+
+public class Ssd {
+
+    private int value;
+
+    public Ssd(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
